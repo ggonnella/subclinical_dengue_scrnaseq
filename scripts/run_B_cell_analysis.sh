e4db2de87721743f8cc0f7f6ccd72dcb14d1b386
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2024-12-16 14:31:56 +0700

    improve the scripts to run all steps

--- scripts/run_B_cell_analysis.sh
+++ scripts/run_B_cell_analysis.sh
@@ -1,12 +1,44 @@
 #!/usr/bin/env bash
+#
+# (c) 2024-2025 BAIA Unit, Institut Pasteur du Cambodge, Phnom Pehn, Cambodia
+#
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PRJ_DIR=$SCRIPT_DIR/..
 ANALYSIS_DIR=$PRJ_DIR/analysis-1
 STEPS_DIR=$ANALYSIS_DIR/steps
 
+if [ "$1" == "" ]; then
+  echo "Run all the steps of the B cell analysis" > /dev/stderr
+  echo "" > /dev/stderr
+  echo "Usage:" > /dev/stderr
+  echo "  $0 <prefix>" > /dev/stderr
+  echo "" > /dev/stderr
+  echo "<prefix>: path to the project directory in the local system" > /dev/stderr
+  echo "" > /dev/stderr
+  exit 1
+fi
+PFX=$1
+
 K2H=$SCRIPT_DIR/knit2html
 
-$K2H $STEPS_DIR/106.0.P.B.merging.seurat/rundir/merging.Rmd
-$K2H $STEPS_DIR/106.0.V.B.post_merging/rundir/post_merging_analysis.Rmd
-$K2H $STEPS_DIR/106.0.V.B.post_merging/rundir/post_merging_res_selection.Rmd
+for rmd in \
+  106.0.P.B.merging.seurat/rundir/merging.Rmd \
+  106.0.V.B.post_merging/rundir/post_merging_analysis.Rmd \
+  106.0.V.B.post_merging/rundir/post_merging_res_selection.Rmd
+do
+  # if rundir does not exist, create it and link the scripts file
+  rundir=$(dirname $STEPS_DIR/$rmd)
+  if [ ! -d $rundir ]; then
+    mkdir -p $rundir
+    cd $rundir
+    rmd_bn=$(basename $rmd)
+    ln -s ../scripts/$rmd_bn .
+    cd -
+  fi
+  # if html file does not exist, run the script
+  rmd_pfx=$(basename $rmd .Rmd)
+  if [ ! -f $rundir/$rmd_pfx.html ]; then
+    $K2H $STEPS_DIR/$rmd pfx=$PFX
+  fi
+done
--- scripts/run_T_cell_analysis.sh
+++ scripts/run_T_cell_analysis.sh
@@ -1,18 +1,51 @@
 #!/usr/bin/env bash
+#
+# (c) 2024-2025 BAIA Unit, Institut Pasteur du Cambodge, Phnom Pehn, Cambodia
+#
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PRJ_DIR=$SCRIPT_DIR/..
 ANALYSIS_DIR=$PRJ_DIR/analysis-1
 STEPS_DIR=$ANALYSIS_DIR/steps
 
+if [ "$1" == "" ]; then
+  echo "Run all the steps of the T cell analysis" > /dev/stderr
+  echo "" > /dev/stderr
+  echo "Usage:" > /dev/stderr
+  echo "  $0 <prefix>" > /dev/stderr
+  echo "" > /dev/stderr
+  echo "<prefix>: path to the project directory in the local system" > /dev/stderr
+  echo "" > /dev/stderr
+  exit 1
+fi
+PFX=$1
+
 K2H=$SCRIPT_DIR/knit2html
 
-$K2H $STEPS_DIR/006.0.P.T.merging.seurat/rundir/merging.Rmd
-$K2H $STEPS_DIR/006.0.V.T.post_merging/rundir/post_merging.Rmd
+for rmd in \
+  006.0.P.T.merging.seurat/rundir/merging.Rmd \
+  006.0.V.T.post_merging/rundir/post_merging.Rmd \
+  \
+  007.0.V.T.post_integration.harmony/rundir/post_harmony_analysis.Rmd \
+  007.0.P.T.integration.harmony/rundir/harmony.Rmd \
+  007.0.P.T.integration.harmony/rundir/post_processing.Rmd \
+  \
+  008.0.V.T.post_split_CD8EM/rundir/post_split_analysis.Rmd \
+  008.0.P.T.split_CD8EM/rundir/split.Rmd
+do
+  # if rundir does not exist, create it and link the scripts file
+  rundir=$(dirname $STEPS_DIR/$rmd)
+  if [ ! -d $rundir ]; then
+    mkdir -p $rundir
+    cd $rundir
+    rmd_bn=$(basename $rmd)
+    ln -s ../scripts/$rmd_bn .
+    cd -
+  fi
+  # if html file does not exist, run the script
+  rmd_pfx=$(basename $rmd .Rmd)
+  if [ ! -f $rundir/$rmd_pfx.html ]; then
+    $K2H $STEPS_DIR/$rmd pfx=$PFX
+  fi
+done
 
-$K2H $STEPS_DIR/007.0.V.T.post_integration.harmony/rundir/post_harmony_analysis.Rmd
-$K2H $STEPS_DIR/007.0.P.T.integration.harmony/rundir/harmony.Rmd
-$K2H $STEPS_DIR/007.0.P.T.integration.harmony/rundir/post_processing.Rmd
-
-$K2H $STEPS_DIR/008.0.V.T.post_split_CD8EM/rundir/post_split_analysis.Rmd
-$K2H $STEPS_DIR/008.0.P.T.split_CD8EM/rundir/split.Rmd
