627a88b1bca27e0ccc77a3c034fc09d4e479cc21
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2025-01-26 13:49:26 +0700

    update scripts to run all steps

--- scripts/run_B_cell_analysis.sh
+++ scripts/run_B_cell_analysis.sh
@@ -8,57 +8,80 @@ PRJ_DIR=$SCRIPT_DIR/..
 ANALYSIS_DIR=$PRJ_DIR/analysis-1
 STEPS_DIR=$ANALYSIS_DIR/steps
 
-if [ "$1" == "" ]; then
+if [ "$2" == "" ]; then
   echo "Run all the steps of the B cell analysis" > /dev/stderr
   echo "" > /dev/stderr
   echo "Usage:" > /dev/stderr
-  echo "  $0 <prefix>" > /dev/stderr
+  echo "  $0 <prefix> <Rlibpath>" > /dev/stderr
   echo "" > /dev/stderr
   echo "<prefix>: path to the project directory in the local system" > /dev/stderr
+  echo "" > /dev/stderr
+  echo "<Rlibpath>: path to the R library containing Seurat 5" > /dev/stderr
   echo "" > /dev/stderr
   exit 1
 fi
 PFX=$1
+LIBPATH=$2
 
 K2H=$SCRIPT_DIR/knit2html
 
+for rundir in \
+  101.0.P.B.create_seurat_obj/rundir/ \
+  101.0.V.B.before_filtering/ \
+  \
+  102.0.P.B.qc_filter/rundir/ \
+  102.0.V.B.qc_filtered/
+do
+  PREVDIR=$PWD
+  # if rundir does not exist, create it and link the scripts file
+  if [ ! -d $rundir ]; then
+    mkdir -p $rundir
+    cd $rundir
+    ln -s ../scripts/* .
+    cd -
+  fi
+  cd $rundir
+  ./run_all_samples.sh
+  cd $PREVDIR
+done
+
 for rmd in \
-  106.0.P.B.merging.seurat/rundir/merging.Rmd \
-  106.0.V.B.merged/rundir/post_merging.Rmd \
+  103.0.P.B.merging.seurat/rundir/merging.Rmd \
+  103.0.V.B.merged/rundir/post_merging.Rmd \
   \
-  107.0.P.B.integration.harmony/rundir/harmony.Rmd \
-  107.0.P.B.integration.harmony/rundir/post_processing.Rmd \
-  107.0.V.B.integrated/rundir/post_harmony_analysis.Rmd \
-  107.0.V.B.integrated/rundir/res_selection.Rmd \
-  107.0.V.B.integrated/rundir/cmp_singleR_refs.Rmd \
+  104.0.P.B.integration.harmony/rundir/harmony.Rmd \
+  104.0.P.B.integration.harmony/rundir/harmony_post_processing.Rmd \
+  104.0.V.B.integrated/rundir/post_harmony_analysis.Rmd \
+  104.0.V.B.integrated/rundir/res_selection.Rmd \
+  104.0.V.B.integrated/rundir/cmp_singleR_refs.Rmd \
   \
-  108.0.P.B.split_plasmablasts/rundir/split.Rmd \
-  108.0.V.B.splitted_plasmablasts/rundir/post_split_analysis.Rmd \
+  105.0.P.B.split_plasmablasts/rundir/split.Rmd \
+  105.0.V.B.splitted_plasmablasts/rundir/post_split_analysis.Rmd \
   \
-  109.0.P.B.scMetabolism/rundir/run_scMetabolism.Rmd \
-  109.0.V.B.metabolism_plots/rundir/scMetabolism_results.Rmd \
+  106.0.P.B.scMetabolism/rundir/run_scMetabolism.Rmd \
+  106.0.V.B.metabolism_plots/rundir/scMetabolism_results.Rmd \
   \
-  110.0.P.B.de_analysis/rundir/compute_de.Rmd \
-  110.0.V.B.de_results/rundir/de_dotplots.Rmd \
-  110.0.V.B.de_results/rundir/deseq_tables_and_vulcanos.Rmd \
+  107.0.P.B.de_analysis/rundir/compute_de.Rmd \
+  107.0.V.B.de_results/rundir/de_dotplots.Rmd \
+  107.0.V.B.de_results/rundir/deseq_tables_and_vulcanos.Rmd \
   \
-  111.0.P.B.gsea_analysis/rundir/compute_gsea.Rmd \
-  111.0.V.B.gsea_results/rundir/gsea_go.Rmd \
-  111.0.V.B.gsea_results/rundir/gsea_kegg.Rmd \
+  108.0.P.B.gsea_analysis/rundir/compute_gsea.Rmd \
+  108.0.V.B.gsea_results/rundir/gsea_go.Rmd \
+  108.0.V.B.gsea_results/rundir/gsea_kegg.Rmd \
   \
-  112.0.P.B.isolate_plasmablasts/rundir/isolate_plasmablasts.Rmd \
-  112.0.V.B.isolated_plasmablasts/rundir/analysis_isolated_plasmablasts.Rmd \
+  109.0.P.B.isolate_plasmablasts/rundir/isolate_plasmablasts.Rmd \
+  109.0.V.B.isolated_plasmablasts/rundir/analysis_isolated_plasmablasts.Rmd \
   \
-  113.0.P.B.BCR_analysis.platypus/rundir/platypus_create.Rmd \
-  113.0.P.B.BCR_analysis.platypus/rundir/post_processing.Rmd \
-  113.0.P.B.BCR_analysis.platypus/rundir/further_post_processing.Rmd \
-  113.0.V.B.BCR_results/rundir/gene_usage.Rmd \
-  113.0.V.B.BCR_results/rundir/shm_analysis.Rmd \
-  113.0.V.B.BCR_results/rundir/cdr3_region.Rmd \
-  113.0.V.B.BCR_results/rundir/clonotypes.Rmd \
+  110.0.P.B.BCR_analysis.platypus/rundir/platypus_create.Rmd \
+  110.0.P.B.BCR_analysis.platypus/rundir/post_processing.Rmd \
+  110.0.P.B.BCR_analysis.platypus/rundir/further_post_processing.Rmd \
+  110.0.V.B.BCR_results/rundir/gene_usage.Rmd \
+  110.0.V.B.BCR_results/rundir/shm_analysis.Rmd \
+  110.0.V.B.BCR_results/rundir/cdr3_region.Rmd \
+  110.0.V.B.BCR_results/rundir/clonotypes.Rmd \
   \
-  114.0.P.B.enclone_preparation/rundir/output_barcode_table.Rmd \
-  114.0.V.B.enclone_plots/rundir/create_enclone_plots.Rmd
+  111.0.P.B.enclone_preparation/rundir/output_barcode_table.Rmd \
+  111.0.V.B.enclone_plots/rundir/create_enclone_plots.Rmd
 do
   # if rundir does not exist, create it and link the scripts file
   rundir=$(dirname $STEPS_DIR/$rmd)
@@ -72,6 +95,6 @@ do
   # if html file does not exist, run the script
   rmd_pfx=$(basename $rmd .Rmd)
   if [ ! -f $rundir/$rmd_pfx.html ]; then
-    $K2H $STEPS_DIR/$rmd pfx=$PFX
+    $K2H $STEPS_DIR/$rmd pfx=$PFX libpath=$LIBPATH
   fi
 done
--- scripts/run_T_cell_analysis.sh
+++ scripts/run_T_cell_analysis.sh
@@ -8,54 +8,77 @@ PRJ_DIR=$SCRIPT_DIR/..
 ANALYSIS_DIR=$PRJ_DIR/analysis-1
 STEPS_DIR=$ANALYSIS_DIR/steps
 
-if [ "$1" == "" ]; then
+if [ "$2" == "" ]; then
   echo "Run all the steps of the T cell analysis" > /dev/stderr
   echo "" > /dev/stderr
   echo "Usage:" > /dev/stderr
-  echo "  $0 <prefix>" > /dev/stderr
+  echo "  $0 <prefix> <Rlibpath>" > /dev/stderr
   echo "" > /dev/stderr
   echo "<prefix>: path to the project directory in the local system" > /dev/stderr
+  echo "" > /dev/stderr
+  echo "<Rlibpath>: path to the R library containing Seurat 5" > /dev/stderr
   echo "" > /dev/stderr
   exit 1
 fi
 PFX=$1
+LIBPATH=$2
 
 K2H=$SCRIPT_DIR/knit2html
 
+for rundir in \
+  001.0.P.T.create_seurat_obj/rundir/ \
+  001.0.V.T.before_filtering/ \
+  \
+  002.0.P.T.qc_filter/rundir/ \
+  002.0.V.T.qc_filtered/
+do
+  PREVDIR=$PWD
+  # if rundir does not exist, create it and link the scripts file
+  if [ ! -d $rundir ]; then
+    mkdir -p $rundir
+    cd $rundir
+    ln -s ../scripts/* .
+    cd -
+  fi
+  cd $rundir
+  ./run_all_samples.sh
+  cd $PREVDIR
+done
+
 for rmd in \
-  006.0.P.T.merging.seurat/rundir/merging.Rmd \
-  006.0.V.T.merged/rundir/post_merging.Rmd \
+  003.0.P.T.merging.seurat/rundir/merging.Rmd \
+  003.0.V.T.merged/rundir/post_merging.Rmd \
   \
-  007.0.P.T.integration.harmony/rundir/harmony.Rmd \
-  007.0.P.T.integration.harmony/rundir/post_processing.Rmd \
-  007.0.V.T.integrated/rundir/post_harmony_analysis.Rmd \
-  007.0.V.T.integrated/rundir/res_selection.Rmd \
-  007.0.V.T.integrated/rundir/cmp_singleR_refs.Rmd \
+  004.0.P.T.integration.harmony/rundir/harmony.Rmd \
+  004.0.P.T.integration.harmony/rundir/harmony_post_processing.Rmd \
+  004.0.V.T.integrated/rundir/post_harmony_analysis.Rmd \
+  004.0.V.T.integrated/rundir/res_selection.Rmd \
+  004.0.V.T.integrated/rundir/cmp_singleR_refs.Rmd \
   \
-  008.0.P.T.split_CD8EM/rundir/split.Rmd \
-  008.0.V.T.splitted_CD8EM/rundir/post_split_analysis.Rmd \
+  005.0.P.T.split_CD8EM/rundir/split.Rmd \
+  005.0.V.T.splitted_CD8EM/rundir/post_split_analysis.Rmd \
   \
-  009.0.P.T.scMetabolism/rundir/run_scMetabolism.Rmd \
-  009.0.V.T.metabolism_plots/rundir/scMetabolism.Rmd \
+  006.0.P.T.scMetabolism/rundir/run_scMetabolism.Rmd \
+  006.0.V.T.metabolism_plots/rundir/scMetabolism.Rmd \
   \
-  010.0.P.T.compute_de/rundir/compute_de.Rmd \
-  010.0.V.T.de_results/rundir/de_dotplots.Rmd \
-  010.0.V.T.de_results/rundir/selected_genes_heatmap.Rmd \
-  010.0.V.T.de_results/rundir/deseq_tables_and_vulcanos.Rmd \
+  007.0.P.T.compute_de/rundir/compute_de.Rmd \
+  007.0.V.T.de_results/rundir/de_dotplots.Rmd \
+  007.0.V.T.de_results/rundir/selected_genes_heatmap.Rmd \
+  007.0.V.T.de_results/rundir/deseq_tables_and_vulcanos.Rmd \
   \
-  011.0.P.T.compute_gsea/rundir/compute_gsea.Rmd \
-  011.0.V.T.gsea_results/rundir/gsea_go.Rmd \
-  011.0.V.T.gsea_results/rundir/gsea_kegg.Rmd \
+  008.0.P.T.compute_gsea/rundir/compute_gsea.Rmd \
+  008.0.V.T.gsea_results/rundir/gsea_go.Rmd \
+  008.0.V.T.gsea_results/rundir/gsea_kegg.Rmd \
   \
-  012.0.P.T.isolate_CD8EM/rundir/isolate_effmemCD8.Rmd \
-  012.0.V.T.isolated_CD8EM/rundir/analysis_isolated_CD8EM.Rmd \
+  009.0.P.T.isolate_CD8EM/rundir/isolate_effmemCD8.Rmd \
+  009.0.V.T.isolated_CD8EM/rundir/analysis_isolated_CD8EM.Rmd \
   \
-  013.0.P.T.TCR_analysis.platypus/rundir/platypus_create.Rmd \
-  013.0.P.T.TCR_analysis.platypus/rundir/post_processing.Rmd \
-  013.0.P.T.TCR_analysis.platypus/rundir/further_post_processing.Rmd \
-  013.0.V.T.TCR_results/rundir/gene_usage.Rmd \
-  013.0.V.T.TCR_results/rundir/cdr3_region.Rmd \
-  013.0.V.T.TCR_results/rundir/clonotypes.Rmd
+  010.0.P.T.TCR_analysis.platypus/rundir/platypus_create.Rmd \
+  010.0.P.T.TCR_analysis.platypus/rundir/post_processing.Rmd \
+  010.0.P.T.TCR_analysis.platypus/rundir/further_post_processing.Rmd \
+  010.0.V.T.TCR_results/rundir/gene_usage.Rmd \
+  010.0.V.T.TCR_results/rundir/cdr3_region.Rmd \
+  010.0.V.T.TCR_results/rundir/clonotypes.Rmd
 do
   # if rundir does not exist, create it and link the scripts file
   rundir=$(dirname $STEPS_DIR/$rmd)
@@ -69,7 +92,7 @@ do
   # if html file does not exist, run the script
   rmd_pfx=$(basename $rmd .Rmd)
   if [ ! -f $rundir/$rmd_pfx.html ]; then
-    $K2H $STEPS_DIR/$rmd pfx=$PFX
+    $K2H $STEPS_DIR/$rmd pfx=$PFX libpath=$LIBPATH
   fi
 done
 
