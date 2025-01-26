55d3585e50aa7294dcef81bf09ee3f9f72ea2c1f
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2025-01-26 13:46:21 +0700

    merging helpers

--- helpers/merging_helpers.R
+++ helpers/merging_helpers.R
@@ -1,7 +1,37 @@
-#
-# Note: code of this function originally written by S.Mella
-#
-prepare_so_merging <- function(so, verbose=F){
+so_version <- function(so) {
+  so_v = substr(so@version, 1, 1)
+  if (!so_v %in% c("4", "5"))
+    stop("Unsupported Seurat version: ", so@version)
+  return(so_v)
+}
+
+prepare_so_merging <- function(so) {
+  # remove all assays except RNA
+  DefaultAssay(so) <- "RNA"
+  for (assay in names(so@assays)) {
+    if (assay != "RNA") {
+      so@assays[[assay]] <- NULL
+    }
+  }
+  if (substr(so@version, 1, 1) == "5") {
+    so@assays$RNA@layers$data <- NULL
+    so@assays$RNA@layers$scale.data <- NULL
+  }
+  # remove SCT results from meta.data
+  so@meta.data <-
+    so@meta.data[-grep(pattern = "SCT", colnames(so@meta.data))]
+  # remove all embeddings
+  for (embedding in names(so@reductions)) {
+    so@reductions[[embedding]] <- NULL
+  }
+  # remove all graphs
+  for (graph in names(so@graphs)) {
+    so@graphs[[graph]] <- NULL
+  }
+  return(so)
+}
+
+prepare_so_merging_Seurat4 <- function(so, verbose=F){
   if (verbose) cat("Changing default assay to 'RNA'\n")
   DefaultAssay(so) <- "RNA"
   if (verbose) cat("Removing SCT assay\n")
