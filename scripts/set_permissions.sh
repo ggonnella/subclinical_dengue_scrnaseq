7b582dd73c8e6afec34c521591e660eb600b89e8
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2025-01-31 15:01:44 +0700

    rename paths

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find /srv/baia/inden -user $USER | xargs chmod g+rwX
-find /srv/baia/inden -user $USER | xargs chgrp baia
+find /srv/baia/prj/subclinical_dengue_scrnaseq -user $USER | xargs chmod g+rwX
+find /srv/baia/prj/subclinical_dengue_scrnaseq -user $USER | xargs chgrp baia
